c53e72fd16e3881927c564899655b5bc46db5074
Emmanuel LESNE <emmanuel.lesne@middleware-solutions.fr>
2020-06-01 21:27:35 +0200

    Partie B_design

---
+++ poms/jobs/process/B_design/Factures_SAP/b2_0.1/src/main/templates/Job_run_template.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+
+CLASSPATH=.:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/log4j-slf4j-impl-2.12.1.jar:$ROOT_PATH/../lib/log4j-api-2.12.1.jar:$ROOT_PATH/../lib/log4j-core-2.12.1.jar:$ROOT_PATH/../lib/crypto-utils.jar:$ROOT_PATH/../lib/slf4j-api-1.7.25.jar:$ROOT_PATH/../lib/dom4j-2.1.1.jar:$ROOT_PATH/b2_0_1.jar:
+JVM_ARGS=-Xms256M -Xmx1024M -Dfile.encoding=UTF-8
+
+java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib \
+  $JVM_ARGS \
+  -cp $CLASSPATH \
+  formation.b2_0_1.B2  --context=Default "$@"
---
+++ poms/jobs/process/B_design/Factures_SAP/factures_sap_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;factures_sap_0_1.jar;b2_0_1.jar;t1_0_1.jar;a1_0_1.jar;b3_0_1.jar;b1_0_1.jar; formation.factures_sap_0_1.Factures_SAP  %*
